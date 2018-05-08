3287861c3727d49fb1e81ff63e13e8cd3c686c64
dolkid <dol.mouse92@gmail.com>
2018-05-08 16:25:18 +0700

    tao khoa chinh va khoa ngoai

--- example_1.sql
+++ example_1.sql
@@ -19,3 +19,18 @@ create table userroles(
     foreign key(user_id) references users(user_id),
     foreign key(role_id) references roles(role_id)
 );
+
+create table customers(
+	id int auto_increment primary key,
+    name varchar(40),
+    address varchar(255),
+    email varchar(255)
+);
+
+create table orders(
+	id int auto_increment,
+	staff varchar(50),
+    primary key(id),
+    customer_id int,
+    foreign key(customer_id) references customers(id)
+);
